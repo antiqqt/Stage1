78a12fa6a5b0cac6d432ecc8313f88f5fcf9f4b4
Anton Pomogaev <antonpomogaev@gmail.com>
2022-04-20 23:11:04 +0300

    feat: add menu on main page according to guidelines

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -3,6 +3,7 @@
 /*
  ** WEEK 1
  */
+
 // Make button on 'Start screen' section
 // lead to the 'Our friends' section
 const sectionStartBtn = document.querySelector('.section--start__button');
@@ -20,37 +21,87 @@ sectionFriendsBtn.addEventListener('click', (e) => {
   window.location.assign('../pets');
 });
 
-// Sidebar menu
-function toggleSidebar() {
-  const sideBar = document.querySelector('.nav--sidebar');
-  sideBar.classList.toggle('nav--sidebar--active');
-}
+/*
+ ** WEEK 3
+ */
 
-function closeSidebar() {
-  const sideBar = document.querySelector('.nav--sidebar');
-  sideBar.classList.remove('nav--sidebar--active');
-}
-
-// Burger button
-const burger = document.querySelector('.burger');
-
-burger.addEventListener('click', toggleSidebar);
-burger.addEventListener('click', toggleBurger);
-
-function toggleBurger() {
-  const burger = document.querySelector('.burger');
-  burger.classList.toggle('burger--active');
-}
-
-function closeBurger() {
-  const burger = document.querySelector('.burger');
-  burger.classList.remove('burger--active');
-}
-
-// Links in navbar
+// Elements
+const sideBar = document.querySelector('.nav--sidebar');
+const burgerBtn = document.querySelector('.burger');
 const navLinks = document.querySelectorAll('.nav__link');
+const overlay = document.querySelector('.overlay');
+const headerLogo = document.querySelector('.header__logo');
+const body = document.querySelector('.body');
+
+// Listeners
+burgerBtn.addEventListener('click', toggleSidebar);
+burgerBtn.addEventListener('click', toggleBurger);
+burgerBtn.addEventListener('click', toggleHeaderLogo);
+burgerBtn.addEventListener('click', toggleOverlay);
+burgerBtn.addEventListener('click', toggleScrollOnBody);
+
 
 navLinks.forEach((elem) => {
   elem.addEventListener('click', closeSidebar);
   elem.addEventListener('click', closeBurger);
+  elem.addEventListener('click', toggleHeaderLogo);
+  elem.addEventListener('click', closeOverlay);
+  elem.addEventListener('click', toggleScrollOnBody);
 });
+
+
+overlay.addEventListener('click', closeSidebar);
+overlay.addEventListener('click', closeBurger);
+overlay.addEventListener('click', toggleHeaderLogo);
+overlay.addEventListener('click', closeOverlay);
+overlay.addEventListener('click', toggleScrollOnBody);
+
+// Sidebar menu
+function toggleSidebar() {
+  sideBar.classList.toggle('nav--sidebar--active');
+}
+
+function closeSidebar() {
+  sideBar.classList.remove('nav--sidebar--active');
+}
+
+// Burger button
+function toggleBurger() {
+  burgerBtn.classList.toggle('burger--active');
+}
+
+function closeBurger() {
+  burgerBtn.classList.remove('burger--active');
+}
+
+// Overlay blackout
+function toggleOverlay() {
+  overlay.classList.toggle('overlay--active');
+}
+
+function closeOverlay() {
+  overlay.classList.remove('overlay--active');
+}
+
+// Logos
+function toggleHeaderLogo() {
+  headerLogo.classList.toggle('logo--hidden');
+}
+
+function closeHeaderLogo() {
+  headerLogo.classList.remove('header__logo--hidden');
+}
+
+// Overlay blackout
+function toggleOverlay() {
+  overlay.classList.toggle('overlay--active');
+}
+
+function closeOverlay() {
+  overlay.classList.remove('overlay--active');
+}
+
+// Scroll
+function toggleScrollOnBody() {
+  body.classList.toggle('body--scroll-disabled')
+}
